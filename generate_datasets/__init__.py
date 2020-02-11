3999f13c8526f1dce8823e8852befd65233506ac
Frederico Vicente <32547732+Mr-Vicente@users.noreply.github.com>
2020-02-11 12:35:13 +0000

    Update __init__.py

--- generate_datasets/__init__.py
+++ generate_datasets/__init__.py
@@ -2,7 +2,7 @@ __version__ = '0.1'
 __author__ = 'Frederico Vicente & Ludwig Krippahl'
 __description__ = 'Library to generate datasets using GAN/VAE'
 
-from nameOfLibrary import (
+from generate_datasets import (
     generate_gan.py,
     generate_vae.py
     classifier.py
