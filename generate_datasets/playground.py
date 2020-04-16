616211938ee009794cb61029199352e3dd2e1f82
Frederico Vicente <32547732+Mr-Vicente@users.noreply.github.com>
2020-04-16 20:36:23 +0100

    PGGAN playground update

--- generate_datasets/playground.py
+++ generate_datasets/playground.py
@@ -11,18 +11,26 @@ Created on Wed Feb 12 08:45:21 2020
 """
 
 import data_access
-import tensorflow as tf
-from wgan_gp_class_big import Big_WGAN
+#import tensorflow as tf
+from PGGAN_class import PGGAN
 
+'''
 BATCH_SIZE = 64
 EPOCHS = 1000
 NOISE_SIZE = 128
+'''
 
-tf.keras.backend.clear_session()
+pggan = PGGAN()
+EPOCHS = [2,2,2,2,2,2]
+pggan.load_dataset(data_access.prepare_data('gan'),2)
+pggan.train(EPOCHS)
 
+
+'''
 gan = Big_WGAN(BATCH_SIZE,NOISE_SIZE)
 gan.load_dataset(data_access.prepare_data('gan',BATCH_SIZE),2)
 
 gan.train_model(EPOCHS)
 gan.generate_images(10,"imgs")
+'''
 
