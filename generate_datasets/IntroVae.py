b9a5074c4a4683c3aff884640ef321e0be15dbdd
Frederico Vicente <32547732+Mr-Vicente@users.noreply.github.com>
2020-04-18 10:12:26 +0100

    Update infoVae training/losses

--- generate_datasets/IntroVae.py
+++ generate_datasets/IntroVae.py
@@ -5,25 +5,27 @@ Created on Tue Apr 14 17:13:25 2020
 @author: MrVicente
 """
 
+import data_access
+
 import tensorflow as tf
-from tensorflow.keras.layers import Dense,Reshape,Conv2D,Flatten,UpSampling2D,LeakyReLU,Cropping2D,AveragePooling2D,BatchNormalization,ZeroPadding2D,ReLU
+from tensorflow.keras.layers import Dense,Reshape,Conv2D,Flatten,UpSampling2D,LeakyReLU,Cropping2D,AveragePooling2D,BatchNormalization,ZeroPadding2D,ReLU,InputLayer
 from tensorflow.keras.optimizers import Adam
 
 import numpy as np
 import time
 
 class ResidualBlock(tf.keras.Model):
-    def __init__(self, input_channels, out_channels, stride=(1,1), downsample=None):
+    def __init__(self, input_channels, out_channels, layer_n = -1,  stride=(1,1), downsample=None):
         super().__init__(name = "Residual_Block")
         if(input_channels != out_channels):
-            self.conv_expand = Conv2D(out_channels,(1,1), strides=stride)
+            self.conv_expand = Conv2D(out_channels, (1,1), padding='same', kernel_initializer='he_normal')
         else:
             self.conv_expand = None
         
-        self.conv_1 = Conv2D(out_channels,(4,4), strides=stride)
+        self.conv_1 = Conv2D(out_channels,(4,4), padding='same', kernel_initializer='he_normal')
         self.batch_norm_1 = BatchNormalization()
         self.leaky_1 = LeakyReLU(alpha=0.2)
-        self.conv_2 = Conv2D(out_channels,(4,4), strides=stride)
+        self.conv_2 = Conv2D(out_channels,(4,4), padding='same', kernel_initializer='he_normal')
         self.batch_norm_2 = BatchNormalization()
         self.leaky_2 = LeakyReLU(alpha=0.2)
         
@@ -32,6 +34,7 @@ class ResidualBlock(tf.keras.Model):
             residual = self.conv_expand(input_tensor)
         else:
             residual = input_tensor
+        out = input_tensor
         out = self.conv_1(input_tensor)
         out = self.batch_norm_1(out)
         out = self.leaky_1(out)
@@ -42,35 +45,43 @@ class ResidualBlock(tf.keras.Model):
         return out
     
 class Generator(tf.keras.Model):
-    def __init__(self, latent_dim = 128, channels=[64,128,256,512,512,512]):
+    def __init__(self, latent_dim = 128, channels=[128,128,128,128,128]):
         super().__init__(name = "Generator")
-        cc = channels[0]
+        cc = channels[-1]
+        self.inp = InputLayer(input_shape=(latent_dim,))
         self.dense_1 = Dense(latent_dim * 5 * 5, name = 'Generator_Dense_1')
         self.relu = ReLU()
         self.reshape_1 = Reshape((5,5,latent_dim))
-        self.res_block_1 = ResidualBlock(cc,cc)
         self.reses = list()
-        for ch in channels[1:]:
-            self.reses.append([UpSampling2D(),ResidualBlock(cc,ch)])
+        count = 0
+        for ch in channels[::-1]:
+            self.reses.append([ResidualBlock(cc,ch,count),UpSampling2D()])
             cc = ch
+            count += 1
+        self.res_block_n = ResidualBlock(cc,cc)
         self.toRGB = Conv2D(3, (5,5), padding='same', kernel_initializer='he_normal', name = 'Generator_To_RGB')
+        self.optimizer = Adam(learning_rate=0.0002,beta_1=0.9,beta_2=0.999)
         
     def call(self, input_tensor):
-        x = self.dense_1(input_tensor)
+        x = self.inp(input_tensor)
+        x = self.dense_1(x)
         x = self.relu(x)
         x = self.reshape_1(x)
-        x = self.res_block_1(x)
         for i in range(len(self.reses)):
             x = self.reses[i][0](x)
             x = self.reses[i][1](x)
+            if(i == 1):
+               x = Cropping2D(cropping=((1,0),(1,0)))(x)
         x = self.toRGB(x)
-        x = tf.keras.layers.Activation(activation='sigmoid')(x)
         return x
+
+    def backPropagate(self,gradients,trainable_variables):
+        self.optimizer.apply_gradients(zip(gradients, trainable_variables))
     
 class Encoder(tf.keras.Model):
-    def __init__(self, channels=[64,128,256,512,512,512]):
+    def __init__(self, channels=[128,128,128,128,128]):
         super().__init__(name = "Encoder")
-        self.conv_1 = Conv2D(128, (4,4),strides=(1,1), input_shape = (152,152,3))
+        self.conv_1 = Conv2D(128, (4,4), padding='same', kernel_initializer='he_normal', input_shape = (152,152,3))
         self.batch_norm_1 = BatchNormalization()
         self.leaky_1 = LeakyReLU(alpha=0.2)
         self.downsampling = AveragePooling2D()
@@ -79,8 +90,9 @@ class Encoder(tf.keras.Model):
         for ch in channels[1:]:
             self.reses.append([ResidualBlock(cc,ch),AveragePooling2D()])
             cc = ch
-        self.res_block_n = ResidualBlock(cc,ch)
+        self.res_block_n = ResidualBlock(cc,cc)
         self.flat = Flatten()
+        self.optimizer = Adam(learning_rate=0.0002,beta_1=0.9,beta_2=0.999)
         
     def call(self, input_tensor):
         out = self.conv_1(input_tensor)
@@ -93,16 +105,17 @@ class Encoder(tf.keras.Model):
         out = self.res_block_n(out)
         out = self.flat(out)
         return out
-        
+
+    def backPropagate(self,gradients,trainable_variables):
+        self.optimizer.apply_gradients(zip(gradients, trainable_variables))
+       
 class IntroVae(tf.keras.Model):
-    def __init__(self, latent_dim = 128, channels=[64,128,256,512,512,512]):
+    def __init__(self, latent_dim = 512, channels=[128,128,128,128,128]):
         super().__init__(name = "Intro_Vae")
         
         self.latent_dim = latent_dim
         self.inference_net = Encoder(channels)
         self.generative_net = Generator(self.latent_dim,channels)
-        
-        self.optimizer = Adam(learning_rate=0.0001,beta_1=0,beta_2=0.9)
         
         self.train_dataset = None
         
@@ -117,24 +130,35 @@ class IntroVae(tf.keras.Model):
     @tf.function
     def sample(self, eps=None):
         if eps is None:
-          eps = tf.random.normal(shape=(100, self.latent_dim))
-        return self.decode(eps, apply_sigmoid=True)
-        
-    def encode(self, x):
-        mean, logvar = tf.split(self.inference_net(x), num_or_size_splits=2, axis=1)
+          eps = tf.random.normal(shape=(8, self.latent_dim))
+        return self.decode(eps, train = False, apply_sigmoid=True)
+
+    @tf.function   
+    def encode(self, x, train = True):
+        mean, logvar = tf.split(self.inference_net(x, training = train), num_or_size_splits=2, axis=1)
         return mean, logvar
 
+    @tf.function
     def reparameterize(self, mean, logvar):
         eps = tf.random.normal(shape=mean.shape)
+        print(mean.shape)
         return eps * tf.exp(logvar * .5) + mean
 
-    def decode(self, z, apply_sigmoid=False):
-        logits = self.generative_net(z)
+    @tf.function
+    def decode(self, z, train = True, apply_sigmoid=False):
+        logits = self.generative_net(z, training = train)
         if apply_sigmoid:
           probs = tf.sigmoid(logits)
           return probs
-
         return logits
+
+    #@tf.function
+    def kl_loss(self,mean, logvar):
+        #a = tf.clip_by_value(tf.exp(logvar), 1e-8, 1-1e-8)
+        return tf.reduce_mean(-0.5 * tf.reduce_sum(1 + logvar - tf.square(mean) - tf.exp(logvar), axis=-1))
+    @tf.function
+    def L_AE(self,gen,real):
+        return tf.reduce_mean(tf.keras.losses.mean_squared_error(real,gen))
 
     def log_normal_pdf(self,sample, mean, logvar, raxis=1):
         log2pi = tf.math.log(2. * np.pi)
@@ -142,28 +166,70 @@ class IntroVae(tf.keras.Model):
             -.5 * ((sample - mean) ** 2. * tf.exp(-logvar) + logvar + log2pi),
             axis=raxis)
         
-    @tf.function
-    def compute_loss(self, x):
-        mean, logvar = self.encode(x)
-        z = self.reparameterize(mean, logvar)
-        x_logit = self.decode(z)
-
-        cross_ent = tf.nn.sigmoid_cross_entropy_with_logits(logits=x_logit, labels=x)
-        logpx_z = -tf.reduce_sum(cross_ent, axis=[1, 2, 3])
-        logpz = self.log_normal_pdf(z, 0., 0.)
-        logqz_x = self.log_normal_pdf(z, mean, logvar)
-        return -tf.reduce_mean(logpx_z + logpz - logqz_x)
+    def compute_loss_inf(self, z, logv_z, z_r, logv_z_r, z_pp, logv_z_pp, gen, real):
+        beta = 0.05
+        alpha = 0.25
+        m = 120
+        eq = alpha * ((m - self.kl_loss(z_r, logv_z_r)) + (m - self.kl_loss(z_pp, logv_z_pp)))
+        return self.kl_loss(z,logv_z) + eq + (beta * self.L_AE(gen,real))
 
     @tf.function
+    def compute_loss_gen(self, mean_r, logv_r, mean_pp, logv_pp, gen, real):
+        beta = 0.05
+        alpha = 0.25
+        x = alpha * (self.kl_loss(mean_r,logv_r) + self.kl_loss(mean_pp, logv_pp))
+        return x + (beta * self.L_AE(gen,real))
+
+    #@tf.function
     def compute_apply_gradients(self,x):
-        with tf.GradientTape() as tape:
-            loss = self.compute_loss(x)
-        gradients = tape.gradient(loss, self.trainable_variables)
-        self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+        with tf.GradientTape(persistent=True) as tape:
+            mean, logvar = self.encode(x)
+            z = self.reparameterize(mean, logvar)
+            x_logit = self.decode(z)
+            z_prior = tf.random.normal(shape=mean.shape)
+            x_p = self.decode(z_prior, train = True)
+
+            mean_r, logvar_r = self.encode(x_logit, train = False)
+            z_r =  self.reparameterize(mean_r, logvar_r)
+            mean_pp, logvar_pp = self.encode(x_p, train = False)
+            z_pp =  self.reparameterize(mean_pp, logvar_pp)
+
+            inf_loss = self.compute_loss_inf(mean, logvar, mean_r , logvar_r , mean_pp, logvar_pp, x_logit, x)
+
+            mean_r, logvar_r = self.encode(x_logit, train = True)
+            z_r =  self.reparameterize(mean_r, logvar_r) 
+            mean_pp, logvar_pp = self.encode(x_p, train = True)
+            z_pp =  self.reparameterize(mean_pp, logvar_pp)
+            
+            gen_loss = self.compute_loss_gen(mean_r,logvar_r, mean_pp, logvar_pp, x_logit, x)
+            #gen_loss = self.compute_loss_gen(z_r, z_pp, x_logit, x)
+
+        gradients_of_inf = tape.gradient(inf_loss, self.inference_net.trainable_variables)
+        gradients_of_gen = tape.gradient(gen_loss, self.generative_net.trainable_variables)
+        self.inference_net.backPropagate(gradients_of_inf, self.inference_net.trainable_variables)
+        self.generative_net.backPropagate(gradients_of_gen, self.generative_net.trainable_variables)
+        return inf_loss, gen_loss
         
     def train_model(self,epochs):
         for epoch in range(1, epochs + 1):
             start_time = time.time()
+            steps = 5000 / 8
+            counter = 0
             for train_x in self.train_dataset:
-                self.compute_apply_gradients(train_x)
+                inf_loss, gen_loss = self.compute_apply_gradients(train_x)
+                print('Step {} of {}'.format(counter,steps))
+                print('Inference Loss: {} ------ Generator Loss: {}'.format(inf_loss,gen_loss))
+                print('-------------------------------------------------')
+                counter += 1
             end_time = time.time()
+            print('Epoch: {}, '
+                    'time elapse for current epoch {}'.format(epoch,
+                                                              end_time - start_time))
+
+    def generate_images(self,number_of_samples=5,directory="imgs"):
+        random_vector_for_generation = tf.random.normal(shape=[number_of_samples, 1024])
+        predictions = self.sample(random_vector_for_generation)
+        data_access.prepare_directory(directory)
+        for i in range(predictions.shape[0]):
+            data_access.store_image(directory,None,predictions[i],i,'vae')
+
