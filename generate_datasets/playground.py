0e8d745516ec0d1fddb7460bfb7be025af7ced9f
Frederico Vicente <32547732+Mr-Vicente@users.noreply.github.com>
2020-05-22 10:21:20 +0100

    update playground

--- generate_datasets/playground.py
+++ generate_datasets/playground.py
@@ -11,46 +11,31 @@ Created on Wed Feb 12 08:45:21 2020
 """
 import argparse
 import data_access
-#import tensorflow as tf
-from PGGAN_class import PGGAN
+from IntroVae import IntroVae
 
-'''
-BATCH_SIZE = 64
-EPOCHS = 1000
-NOISE_SIZE = 128
-'''
 
-'''
-gan = Big_WGAN(BATCH_SIZE,NOISE_SIZE)
-gan.load_dataset(data_access.prepare_data('gan',BATCH_SIZE),2)
-
-gan.train_model(EPOCHS)
-gan.generate_images(10,"imgs")
-'''
 def main(args):
-    '''
-    introVae = IntroVae()
-    introVae.load_dataset(data_access.prepare_data('vae'))
-    introVae.train(args.epochs)
-    '''
-    pggan = PGGAN()
-    e = args.epochs
-    EPOCHS = [e,e,e,e,e,e]
-    pggan.load_dataset(data_access.prepare_data('gan'),2)
-    pggan.train(EPOCHS)
+    
+    vae = IntroVae()
+    vae.load_dataset(data_access.prepare_data('vae'))
+    
+    vae.train_model()
+    n_images = 10
+    vae.generate_images(n_images,"imgs")
 
 
 
 if __name__ == '__main__':
     
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add('--batchsz', type=int, default=8, help='batch size')
-    arg_parser.add('--z_dim', type=int, default=128, help='Latent dimension / Noise size')
-    arg_parser.add('--epochs', type=int, default=500, help='Learning iterations through the dataset')
-    arg_parser.add('--alpha', type=float, default=0.25, help='Control weight of adversarial loss (alpha * adv_loss)')
-    arg_parser.add('--beta', type=float, default=0.5, help='Control weight of encoder loss (beta * ae_loss)')
-    arg_parser.add('--gamma', type=float, default=1.0, help='gamma')     
-    arg_parser.add('--lr', type=float, default=0.0002, help='learning rate value') 
+    arg_parser.add_argument('--batchsz', type=int, default=8, help='batch size')
+    arg_parser.add_argument('--z_dim', type=int, default=128, help='Latent dimension / Noise size')
+    arg_parser.add_argument('--epochs', type=int, default=500, help='Learning iterations through the dataset')
+    arg_parser.add_argument('--m', type=float, default=120, help='Margin')
+    arg_parser.add_argument('--alpha', type=float, default=0.25, help='Control weight of adversarial loss (alpha * adv_loss)')
+    arg_parser.add_argument('--beta', type=float, default=0.5, help='Control weight of encoder loss (beta * ae_loss)')
+    arg_parser.add_argument('--gamma', type=float, default=1.0, help='gamma')     
+    arg_parser.add_argument('--lr', type=float, default=0.0002, help='learning rate value') 
     
     args = arg_parser.parse_args()
     main(args)
