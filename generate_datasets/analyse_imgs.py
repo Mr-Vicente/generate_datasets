c6ab647c0e3597d4b8fd0d4ce08347f5599f0095
Frederico Vicente <32547732+Mr-Vicente@users.noreply.github.com>
2020-06-03 11:37:47 +0100

    Updated GUI functioning

--- generate_datasets/analyse_imgs.py
+++ generate_datasets/analyse_imgs.py
@@ -18,18 +18,20 @@ import data_access
 class ManualClassifier:
     def __init__(self,root=None,classifier_name='./classifiers/hair_classifier_v09933.h5',size_shape=(128,128), \
                  class_names=['Blond-yellow','Yellow','Orange','Orange-Brown','Blond','Light-Brown','Brown','Black','Gray','White'], \
-                 images_path = r"C:\Users\MrVicente\Desktop\train_images_sample" , attribute= 'hair_color'):
+                 images_path = r"C:\Users\MrVicente\Desktop\train_images_sample" , attribute= 'hair_color',dataset_name='dataset'):
         canvas = tk.Canvas(root,width=300,height=300)
         canvas.pack()
         
         self.root = root
-        self.classifier = tf.keras.models.load_model('./classifiers/hair_classifier_v09933.h5')
+        self.classifier = tf.keras.models.load_model(classifier_name)
         self.size_shape = size_shape
         self.class_names = class_names
         self.images_path = images_path
         self.attribute = attribute
+        self.dataset_name = dataset_name
         
         self.real_images = list()
+        self.images_to_store = list()
         self.labels = list()
         self.ImageTk_images = list()
         self.index = 0
@@ -51,16 +53,22 @@ class ManualClassifier:
     def changeImage(self,n_class,class_max):
         one_hot = np.zeros(class_max)
         one_hot[n_class] = 1
-        self.labels.append(one_hot)
         print(one_hot)
         if(self.index < len(self.real_images)-1):
+            prediction_i = np.argmax(self.predictions[self.index])
+            if(n_class != prediction_i):
+                self.labels.append(one_hot)
+                self.images_to_store.append(self.real_images[self.index])
             self.index += 1
             prediction_i = np.argmax(self.predictions[self.index])
-            self.panel.configure(image=self.ImageTk_images[self.index],text = self.class_names[prediction_i])
+            self.panel.configure(image=self.ImageTk_images[self.index],text = 'Predicted: ' + self.class_names[prediction_i])
         else:
-            ls = np.array(self.labels)
-            np.savez('{}_{}_{}x{}_{}.npz'.format('cartoon_images',self.attribute,self.size_shape[0],self.size_shape[1],'cc'),images=self.real_images)
-            np.savez('{}_{}_{}x{}_{}.npz'.format('cartoon_labels',self.attribute,self.size_shape[0],self.size_shape[1],'cc'),labels=ls)
+            ls = np.array(self.labels).astype(np.float32)
+            self.images_to_store = np.array(self.images_to_store).astype(np.float32)
+            print('labels to store: ', ls.shape)
+            print('imgs to store: ', self.images_to_store.shape)
+            np.savez('{}_images_{}_{}x{}_{}.npz'.format(self.dataset_name,self.attribute,self.size_shape[0],self.size_shape[1],'cc'),images=self.images_to_store)
+            np.savez('{}_labels_{}_{}x{}_{}.npz'.format(self.dataset_name,self.attribute,self.size_shape[0],self.size_shape[1],'cc'),labels=ls)
 
 
     def draw_buttons(self):
@@ -71,7 +79,7 @@ class ManualClassifier:
 
     def draw_gui(self):
         prediction_i = np.argmax(self.predictions[self.index])
-        self.panel = tk.Label(self.root, image= self.ImageTk_images[self.index],text = self.class_names[prediction_i], compound=tk.RIGHT)
+        self.panel = tk.Label(self.root, image= self.ImageTk_images[self.index],text = 'Predicted: ' + self.class_names[prediction_i], compound=tk.RIGHT)
         self.panel.pack(side="right", fill="both", expand="yes")
         
         self.draw_buttons()
@@ -81,16 +89,18 @@ def main(args):
     
     root = tk.Tk()
     root.wm_title("Manual Classifier")
-    gui = ManualClassifier(root,args.classifier_name,args.shape_size)
+    class_names = ['1 waggon', '2 waggons', '3 waggons', '4 waggons']
+    path= r"C:\Users\MrVicente\Desktop\train_images"
+    gui = ManualClassifier(root,args.classifier_name,args.shape_size, class_names = class_names,images_path=path,attribute='n-waggons')
     gui.draw_gui()
     root.mainloop()
-
+    #'./classifiers/hair_classifier_v09933.h5'
 
 if __name__ == '__main__':
     
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument('--classifier_name', type=str, default='./classifiers/hair_classifier_v09933.h5', help='saved classifier model filename')
-    arg_parser.add_argument('--shape_size', type=tuple, default=(128,128), help='batch size')
+    arg_parser.add_argument('--classifier_name', type=str, default='./classifiers/Number_Of_Wagons_0.9952_ConvExp14_t4000_v1000.hdf5', help='saved classifier model filename')
+    arg_parser.add_argument('--shape_size', type=tuple, default=(152,152), help='batch size')
 
     args = arg_parser.parse_args()
     main(args)
